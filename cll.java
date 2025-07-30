09a5738d23cf574bf3e95ca52a186a6cbd20708d
saisireesha <nirmalasireesha1978@gmail.com>
2025-07-30 11:16:00 +0530

    first_commit

--- cll.java
+++ cll.java
@@ -7,30 +7,100 @@ class ListNode {
         this.next = null;
     }
 }
-class LinkedList{
+
+class LinkedList {
     ListNode head;
-    void traversal(){
+
+    void traversal() {
+        if (head == null) {
+            return;
+        }
+        ListNode temp = head;
+        do {
+            System.out.print(temp.data + " --> ");
+            temp = temp.next;
+        } while (temp != head);
+        System.out.println("(back to head)");
+    }
+
+    void insertatbeg(int data) {
+        ListNode newnode = new ListNode(data);
+
+        if (head == null) {
+            newnode.next = newnode; // point to itself
+            head = newnode;
+            return; // exit early
+        }
+
+        ListNode temp = head;
+        while (temp.next != head) {
+            temp = temp.next;
+        }
+        temp.next = newnode;
+        newnode.next = head;
+        head = newnode;
+    }
+    void insertatend(int data) {
+        ListNode newnode = new ListNode(data);
+
+        if (head == null) {
+            newnode.next = newnode; // point to itself
+            head = newnode;
+            return; // exit early
+        }
+
+        ListNode temp = head;
+        while (temp.next != head) {
+            temp = temp.next;
+        }
+        temp.next = newnode;
+        newnode.next = head;
+    }
+    void deleteatbeg(int data){
         if(head==null){
-        System.out.print("empty");
+            System.out.println("Linked list is empty");
+        }
+        if(head.next==null){
+            head=null;
+            return;
+        }
+        ListNode temp=head;
+        while(temp.next!=head){
+            temp=temp.next;
+        }
+        temp.next=head.next;
+        head=temp.next;
+
+    }
+    void deleteatend(int data){
+        if(head==null){
+            System.out.println("Linked list is empty");
+        }
+        if(head.next==head){
+            head=null;
+            return;
+        }
+        ListNode temp=head;
+        while(temp.next.next!=head){
+            temp=temp.next;
+        }
+        temp.next=head;
         return;
+
+        }
     }
-    ListNode temp=head;
-    do{
-        System.out.print(temp.data+"-->");
-        temp=temp.next;
-    }while(temp!=head);
 
-    System.out.println("null");
 
-    }
-    
-    
-
-}
 public class cll {
-    public static void main(String[]args){
-        LinkedList l=new LinkedList();
-        l.traversal();
+    public static void main(String[] args) {
+        LinkedList l = new LinkedList();
+        l.traversal(); 
+        l.insertatbeg(10);
+        l.insertatbeg(20);
+        l.insertatbeg(30);
+        l.insertatend(40);
+        l.deleteatbeg(30);
+        l.deleteatend(40);
+        l.traversal(); 
     }
-    
 }
